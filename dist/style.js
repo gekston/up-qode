9be5279e5b53c07868731cf2569bc0c55e32ab6e
Nebeschenko Igor <nebeschenkoichor@gmail.com>
2017-10-07 16:50:23 +0300

    slider width

--- dist/style.js
+++ dist/style.js
@@ -1,15 +1,21 @@
 document.addEventListener("DOMContentLoaded", function(){
 
-  let slideCount =  document.querySelectorAll('.slider-list__item').length,
+  let slideCollection = document.querySelectorAll('.slider-list__item'),
+      slideCount =  document.querySelectorAll('.slider-list__item').length,
       slideWidth =  document.querySelector('.slider-list__item').offsetWidth,
       slideHeight = document.querySelector('.slider-list__item').offsetHeight,
       lastSlider = document.querySelector('.slider-list .slider-list__item:last-child'),
       slideList = document.querySelector('.slider-list'),
+      getWport = document.documentElement.clientWidth,
       sliderUlWidth = slideCount * slideWidth;
     slideList.style.width = sliderUlWidth + 'px';
     slideList.style.height = slideHeight + 'px';
     slideList.style.marginLeft = - slideWidth + 'px';
     slideList.insertBefore(lastSlider, slideList.children[0]);
+
+    for(let x = 0; x < slideCount; x++){
+      slideCollection[x].style.width = getWport + 'px';
+    }
 
     function slideLeft() {
         let start = Date.now();
@@ -17,7 +23,7 @@ document.addEventListener("DOMContentLoaded", function(){
             let timePassed = Date.now() - start;
             slideList.style.left = timePassed - slideWidth + 'px';
             if(slideList.style.left > '0px') {
-                slideList.style.left = 0;
+                slideList.style.left = 0 + 'px';
             }
             if (timePassed > 2000) clearInterval(timer);
         }, 20);
@@ -31,7 +37,7 @@ document.addEventListener("DOMContentLoaded", function(){
             let timePassed = Date.now() - start;
             slideList.style.left = - timePassed + slideWidth + 'px';
             if(slideList.style.left < '0px') {
-                slideList.style.left = 0;
+                slideList.style.left = 0 + 'px';
             }
             if (timePassed > 2000) clearInterval(timer);
         }, 20);
